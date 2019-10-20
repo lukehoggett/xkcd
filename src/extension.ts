c38c383a6f21abc2c02d31a9734cfb7afc91b340
Luke Hoggett <luke.hoggett@gmail.com>
2019-10-21 08:05:52 +1100

    modified template sample to open xkcd webview

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  context.subscriptions.push(
+    vscode.commands.registerCommand("xkcd.start", () => {
+      // Create and show panel
+      const panel = vscode.window.createWebviewPanel(
+        "xkcd",
+        "XKCD",
+        vscode.ViewColumn.One,
+        {}
+      );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "xkcd" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-
-	context.subscriptions.push(disposable);
+      // And set its HTML content
+      panel.webview.html = getWebviewContent();
+    })
+  );
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+function getWebviewContent() {
+  return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>XKCD</title>
+		<style>
+			iframe {
+				width: 100%;
+				height: 400px;
+
+			}
+		</style>
+</head>
+<body>
+	<iframe src="https://xkcd.com" />
+</body>
+</html>`;
+}
